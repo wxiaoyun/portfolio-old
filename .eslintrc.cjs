24560abd80544030f7276e1523cd2dd53258ad89
ForAeons <w.xy020203@gmail.com>
2023-09-09 11:44:11 +0800

    update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,39 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2020: true,
   },
-}
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "react-refresh"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/method-signature-style": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react-refresh/only-export-components": "warn",
+  },
+  ignorePatterns: [
+    ".eslintrc.cjs",
+    "postcss.config.js",
+    "tailwind.config.js",
+    "vite.config.ts",
+  ],
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the version of React in your project
+    },
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito"],
+      },
+      keyframes: {
+        shake: {
+          "10%, 90%": { transform: "translate3d(-1px, 0, 0)" },
+          "20%, 80%": { transform: "translate3d(2px, 0, 0)" },
+          "30%, 50%, 70%": { transform: "translate3d(-4px, 0, 0)" },
+          "40%, 60%": { transform: "translate3d(4px, 0, 0)" },
+        },
+        wiggle: {
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(7deg)" },
+          "50%": { transform: "rotate(0deg)" },
+          "75%": { transform: "rotate(-7deg)" },
+        },
+        weakping: {
+          "75%": { transform: "scale(1)" },
+          "100%": { transform: "scale(1.5)", opacity: 0 },
+        },
+        slideDown: {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        slideUp: {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        shake: "shake 0.5s cubic-bezier(.36,.07,.19,.97) infinite",
+        wiggle: "wiggle 0.3s cubic-bezier(.36,.07,.19,.97) infinite",
+        weakping: "weakping 1.5s cubic-bezier(.36,.07,.19,.97) infinite",
+        slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+        slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+      },
+    },
   },
   plugins: [],
 };
