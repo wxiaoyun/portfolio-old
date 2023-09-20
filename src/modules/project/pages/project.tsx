cc7a8f36d9c9ea4e397a3a6915b13db92b3e8a6c
ForAeons <w.xy020203@gmail.com>
2023-09-20 14:37:58 +0800

    animation refinement and projects tab

---
+++ src/modules/project/pages/project.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { CommunityLib } from "..";
+
+export const Project: React.FC = () => {
+	return (
+		<div className="flex flex-col gap-y-24 items-center">
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl justify-self-start max-w-2xl w-full">
+				Projects
+			</h1>
+
+			<CommunityLib />
+		</div>
+	);
+};
+
+export default Project;
