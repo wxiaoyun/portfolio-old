a30921e736444a443e59eafeeadadcc326af41c1
ForAeons <w.xy020203@gmail.com>
2023-10-04 13:52:34 +0800

    add project and refactor

--- src/modules/cca/pages/cca.tsx
+++ src/modules/cca/pages/cca.tsx
@@ -3,7 +3,7 @@ import { CvwoExco } from "../components";
 
 export const CCA: React.FC = () => {
   return (
-    <div className="flex flex-col gap-y-12 items-center mx-6">
+    <div className="flex flex-col gap-y-12 items-center mx-6 w-full">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl self-start">
         CCAs
       </h1>
--- src/modules/education/pages/education.tsx
+++ src/modules/education/pages/education.tsx
@@ -3,7 +3,7 @@ import { Bachelor, NUSCollege } from "..";
 
 export const Education: React.FC = () => {
   return (
-    <div className="flex flex-col gap-y-12 items-center mx-6">
+    <div className="flex flex-col gap-y-12 items-center mx-6 w-full">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl self-start">
         Education
       </h1>
