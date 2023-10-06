d915071df0a61582b2aacd9e357b02a796b39775
ForAeons <w.xy020203@gmail.com>
2023-10-06 15:33:00 +0800

    reduce slide animation

--- src/modules/aboutme/components/toolsandtech.tsx
+++ src/modules/aboutme/components/toolsandtech.tsx
@@ -7,26 +7,22 @@ import { ToolMatrix } from "..";
 export const ToolsAndTech: React.FC = () => {
   return (
     <motion.div
-      className="w-full"
+      className="w-full flex flex-col gap-3"
       initial="offscreen"
       whileInView="onscreen"
       viewport={{ once: true, amount: 0.4 }}
+      variants={fadeSlideInVariants}
     >
-      <motion.div
-        variants={fadeSlideInVariants}
-        className="flex flex-col gap-3"
-      >
-        <div className="text-lg font-semibold w-full">
-          Tools and Technologies that I use
-        </div>
+      <div className="text-lg font-semibold w-full">
+        Tools and Technologies that I use
+      </div>
 
-        <div className="self-center mt-6">
-          <ToolMatrix />
-          <p className="text-sm text-center text-muted-foreground mt-6">
-            Try dragging the icons :P
-          </p>
-        </div>
-      </motion.div>
+      <div className="self-center mt-6">
+        <ToolMatrix />
+        <p className="text-sm text-center text-muted-foreground mt-6">
+          Try dragging the icons :P
+        </p>
+      </div>
     </motion.div>
   );
 };
