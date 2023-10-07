60f75b2ed55eb608d9fc9c6bf9974eac932dbc62
ForAeons <w.xy020203@gmail.com>
2023-10-07 11:45:05 +0800

    change avatar

--- src/modules/aboutme/components/introduction.tsx
+++ src/modules/aboutme/components/introduction.tsx
@@ -1,49 +1,56 @@
 import React from "react";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { AnimatePresence, motion } from "framer-motion";
+import { useTheme } from "@/components/theme-provider";
 
 export const Introduction: React.FC = () => {
-	const [src, setSrc] = React.useState("/self_portrait_low_res.jpg");
+  const theme = useTheme();
+  const ptSrc =
+    theme.theme === "light"
+      ? "/vector_portrait_light.png"
+      : "/vector_portrait_dark.png";
 
-	React.useEffect(() => {
-		const highResImage = new Image();
-		highResImage.src = "/self_portrait_zoomed.JPEG";
-		highResImage.onload = () => setSrc("/self_portrait_zoomed.JPEG");
-	}, []);
-	return (
-			<div
-				className="h-[80vh] mt-[10vh] flex flex-col justify-center items-center gap-6"
-			>
-				<div className="flex w-full justify-around items-center gap-6">
-					<Avatar className="h-52 w-52 shadow-xl">
-						<AvatarImage src={src} alt="portrait" />
-						<AvatarFallback>Xiaoyun</AvatarFallback>
-					</Avatar>
-					<div>
-						<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-							Hello.
-						</h1>
-						<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-							I am Xiaoyun
-						</h1>
-					</div>
-				</div>
+  return (
+    <div className="h-[80vh] mt-[10vh] flex flex-col justify-center items-center gap-6">
+      <div className="flex w-full justify-around items-center gap-6">
+        <div className="relative h-52 w-52">
+          <AnimatePresence mode="wait">
+            <motion.img
+              className="absolute aspect-square transition-shadow duration-500 shadow-xl hover:shadow-2xl rounded-full overflow-hidden"
+              key={ptSrc}
+              src={ptSrc}
+              alt="vector avatar"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeIn" }}
+            />
+          </AnimatePresence>
+        </div>
+        <div>
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Hello.
+          </h1>
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            I am Xiaoyun
+          </h1>
+        </div>
+      </div>
 
-				<p className="leading-7 [&:not(:first-child)]:mt-6">
-					I am a Year 2 undergraduate studying at the National University of
-					Singapore, learning full stack web development.
-				</p>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        I am a Year 2 undergraduate studying at the National University of
+        Singapore, learning full stack web development.
+      </p>
 
-				<p className="leading-7 [&:not(:first-child)]:mt-6">
-					I am an enthusiastic and resilient learner with a passion for
-					collaborative work. Embracing challenges is second nature to me, as I
-					constantly seek opportunities to acquire new skills and knowledge
-					beyond my comfort zone. What sets me apart is my ability to infuse
-					humor and friendliness into the team dynamics, creating a positive and
-					enjoyable environment for everyone involved.
-				</p>
-			</div>
-	);
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        I am an enthusiastic and resilient learner with a passion for
+        collaborative work. Embracing challenges is second nature to me, as I
+        constantly seek opportunities to acquire new skills and knowledge beyond
+        my comfort zone. What sets me apart is my ability to infuse humor and
+        friendliness into the team dynamics, creating a positive and enjoyable
+        environment for everyone involved.
+      </p>
+    </div>
+  );
 };
 
 export default Introduction;
